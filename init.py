66fa4bb0fdb3556a783e8768dbe32f258c614e2a
xez7082 <107535456+xez7082@users.noreply.github.com>
2026-04-03 13:19:09 +0200

    Update init.py

--- init.py
+++ init.py
@@ -1,2 +1,22 @@
-async def async_setup_entry(hass, entry):
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up StorCube from config entry."""
+
+    hass.data.setdefault(DOMAIN, {})
+
+    # exemple coordinator déjà créé ailleurs
+    coordinator = await create_coordinator(hass, entry)
+
+    hass.data[DOMAIN][entry.entry_id] = {
+        "coordinator": coordinator,
+    }
+
+    await hass.config_entries.async_forward_entry_setups(
+        entry, ["sensor"]
+    )
+
     return True
